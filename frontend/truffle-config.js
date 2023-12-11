ff1fe8c80e2339e7ca1b191b46b80bea086936e7
MbitVlad <128845568+MbitVlad@users.noreply.github.com>
2023-12-12 00:29:41 +0200

    fix

--- frontend/truffle-config.js
+++ frontend/truffle-config.js
@@ -1,24 +1,24 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-const mnemonic = "d5ef7d6d4927a3641fb569eea175fc70113568426fd4098a0294ed473adf313f";
+const mnemonic = "1a82cdd916d8d98ef0eb11e39af39840417c032164a8c91f25f751559397a344";
 
 module.exports = {
   contracts_build_directory: "./public/contracts",
   networks: {
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
+     port: 9545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
     sepolia: {
       provider: function () {
-        return new HDWalletProvider(mnemonic, 'wss://wild-maximum-wind.ethereum-sepolia.quiknode.pro/12497e6874c137f4eb46dc28f7e574c54f51925a')
+        return new HDWalletProvider(mnemonic, 'https://wild-maximum-wind.ethereum-sepolia.quiknode.pro/12497e6874c137f4eb46dc28f7e574c54f51925a')
       },
-      network_id: 11155111,
-      // gas: 4000000, // Adjust the gas limit as per your requirements
-      // gasPrice: 10000000000, // Set the gas price to an appropriate value
-      // confirmations: 2, // Set the number of confirmations needed for a transaction
-      // timeoutBlocks: 200, // Set the timeout for transactions
-      // skipDryRun: true // Skip the dry run option
+      network_id: 4868603,
+      gas: 4000000, // Adjust the gas limit as per your requirements
+      gasPrice: 10000000000, // Set the gas price to an appropriate value
+      confirmations: 2, // Set the number of confirmations needed for a transaction
+      timeoutBlocks: 200, // Set the timeout for transactions
+      skipDryRun: true // Skip the dry run option
     }
   },
   compilers: {
